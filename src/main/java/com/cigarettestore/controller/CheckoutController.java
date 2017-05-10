edd48756dd8242750203cdb833660fe15412a382
Giorgio Vitiello <igiorgio94@gmail.com>
2017-05-10 11:58:11 +0200

    Checkout beta

--- src/main/java/com/cigarettestore/controller/CheckoutController.java
+++ src/main/java/com/cigarettestore/controller/CheckoutController.java
@@ -7,7 +7,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cigarettestore.domain.BillingAddress;
@@ -16,12 +18,14 @@ import com.cigarettestore.domain.Payment;
 import com.cigarettestore.domain.ShippingAddress;
 import com.cigarettestore.domain.ShoppingCart;
 import com.cigarettestore.domain.User;
+import com.cigarettestore.domain.UserBilling;
 import com.cigarettestore.domain.UserPayment;
 import com.cigarettestore.domain.UserShipping;
 import com.cigarettestore.service.BillingAddressService;
 import com.cigarettestore.service.CartItemService;
 import com.cigarettestore.service.PaymentService;
 import com.cigarettestore.service.ShippingAddressService;
+import com.cigarettestore.service.UserPaymentService;
 import com.cigarettestore.service.UserService;
 import com.cigarettestore.utility.USConstants;
 
@@ -52,20 +56,125 @@ public class CheckoutController {
 		return "shop-checkout1";
 	}
 	
-	@RequestMapping("/shop-checkout2")
-	public String ShopCheckout2() {
+	@RequestMapping(value="/shop-checkout2", method=RequestMethod.POST)
+	public String ShopCheckout2(
+			@ModelAttribute("firstname") String firstname,
+			@ModelAttribute("lastname") String lastname,
+			@ModelAttribute("company") String company,
+			@ModelAttribute("street") String street,
+			@ModelAttribute("city") String city,
+			@ModelAttribute("zip") String zip,
+			@ModelAttribute("state") String state,
+			@ModelAttribute("country") String country,
+			@ModelAttribute("phone") String phone,
+			@ModelAttribute("email") String email,
+			Principal principal, Model model) {
+		
+		model.addAttribute("firstname", firstname);
+		model.addAttribute("lastname", lastname);
+		model.addAttribute("company", company);
+		model.addAttribute("street", street);
+		model.addAttribute("city", city);
+		model.addAttribute("zip", zip);
+		model.addAttribute("state", state);
+		model.addAttribute("country", country);
+		model.addAttribute("phone", phone);
+		model.addAttribute("email", email);
+		
+		
 		return "shop-checkout2";
 	}
 	
 	@RequestMapping("/shop-checkout3")
-	public String ShopCheckout3() {
+	public String ShopCheckout3(
+			@ModelAttribute("firstname") String firstname,
+			@ModelAttribute("lastname") String lastname,
+			@ModelAttribute("company") String company,
+			@ModelAttribute("street") String street,
+			@ModelAttribute("city") String city,
+			@ModelAttribute("zip") String zip,
+			@ModelAttribute("state") String state,
+			@ModelAttribute("country") String country,
+			@ModelAttribute("phone") String phone,
+			@ModelAttribute("email") String email,
+			@ModelAttribute("payment") String payment,
+			Principal principal, Model model) {
+		
+		model.addAttribute("firstname", firstname);
+		model.addAttribute("lastname", lastname);
+		model.addAttribute("company", company);
+		model.addAttribute("street", street);
+		model.addAttribute("city", city);
+		model.addAttribute("zip", zip);
+		model.addAttribute("state", state);
+		model.addAttribute("country", country);
+		model.addAttribute("phone", phone);
+		model.addAttribute("email", email);
+		model.addAttribute("payment", payment);
+		
 		return "shop-checkout3";
 	}
 	
+	
 	@RequestMapping("/shop-checkout4")
-	public String ShopCheckout4() {
+	public String ShopCheckout4(
+			@RequestParam("id") Long CartId,
+			@ModelAttribute("firstname") String firstname,
+			@ModelAttribute("lastname") String lastname,
+			@ModelAttribute("company") String company,
+			@ModelAttribute("street") String street,
+			@ModelAttribute("city") String city,
+			@ModelAttribute("zip") String zip,
+			@ModelAttribute("state") String state,
+			@ModelAttribute("country") String country,
+			@ModelAttribute("phone") String phone,
+			@ModelAttribute("email") String email,
+			@ModelAttribute("payment") String payment,
+			Principal principal, Model model) {
+		
+		model.addAttribute("firstname", firstname);
+		model.addAttribute("lastname", lastname);
+		model.addAttribute("company", company);
+		model.addAttribute("street", street);
+		model.addAttribute("city", city);
+		model.addAttribute("zip", zip);
+		model.addAttribute("state", state);
+		model.addAttribute("country", country);
+		model.addAttribute("phone", phone);
+		model.addAttribute("email", email);
+		model.addAttribute("payment", payment); 
+
+		
+		User user = userService.findByUsername(principal.getName());
+		
+		UserBilling userBilling = new UserBilling();
+		UserShipping userShipping = new UserShipping();
+		UserPayment userPayment = new UserPayment();
+		
+		
+		model.addAttribute("userShipping", userShipping);
+		model.addAttribute("userBilling", userBilling);
+		model.addAttribute("userPayment", userPayment);
+		
+		List<String> stateList = USConstants.listOfUSStatesCode;
+		Collections.sort(stateList);
+		/*model.addAttribute("orderList", user.orderList());*/
+		
+//		List<String> stateList = USConstants.listOfUSStatesCode;
+//			Collections.sort(stateList);
+			model.addAttribute("stateList", stateList);
+			model.addAttribute("userPaymentList", user.getUserPaymentList());
+			model.addAttribute("userShippingList", user.getUserShippingList());
+			/*model.addAttribute("orderList", user.orderList());*/
+		
 		return "shop-checkout4";
 	}
+	
+	@RequestMapping("/shop-checkout5")
+	public String ShopCheckout5() {
+		return "shop-checkout5";
+	}
+	
 	
 	@RequestMapping("/checkout")
 	public String checkout(
