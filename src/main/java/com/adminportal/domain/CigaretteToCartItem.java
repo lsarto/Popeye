5240e196726923d405d4ad8ae9e4fdfd34892e71
ANDREA SARTO <4and2ea3@gmail.com>
2017-05-15 13:14:32 +0200

    Aggiornati domain in adminportal

---
+++ src/main/java/com/adminportal/domain/CigaretteToCartItem.java
@@ -0,0 +1,51 @@
+package com.adminportal.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class CigaretteToCartItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="cigarette_id")
+	private Cigarette cigarette;
+	
+	@ManyToOne
+	@JoinColumn(name="cart_item_id")
+	private CartItem cartItem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cigarette getCigarette() {
+		return cigarette;
+	}
+
+	public void setCigarette(Cigarette cigarette) {
+		this.cigarette = cigarette;
+	}
+
+	public CartItem getCartItem() {
+		return cartItem;
+	}
+
+	public void setCartItem(CartItem cartItem) {
+		this.cartItem = cartItem;
+	}
+
+	
+	
+}
--- src/main/java/com/adminportal/domain/User.java
+++ src/main/java/com/adminportal/domain/User.java
@@ -1,7 +1,9 @@
 package com.adminportal.domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -12,10 +14,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.adminportal.domain.UserPayment;
+import com.adminportal.domain.UserShipping;
 import com.adminportal.domain.security.Authority;
 import com.adminportal.domain.security.UserRole;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -37,9 +42,20 @@ public class User implements UserDetails{
 	private String phone;
 	private boolean enabled=true;
 	
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+	private List<UserShipping> userShippingList;
+	
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+	private List<UserPayment> userPaymentList;
+	
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JsonIgnore
 	private Set<UserRole> userRoles = new HashSet<>();
+	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+	private ShoppingCart shoppingCart;
 	
 	public Long getId() {
 		return id;
@@ -93,6 +109,21 @@ public class User implements UserDetails{
 	public void setUserRoles(Set<UserRole> userRoles) {
 		this.userRoles = userRoles;
 	}
+	
+	
+	
+	public List<UserShipping> getUserShippingList() {
+		return userShippingList;
+	}
+	public void setUserShippingList(List<UserShipping> userShippingList) {
+		this.userShippingList = userShippingList;
+	}
+	public List<UserPayment> getUserPaymentList() {
+		return userPaymentList;
+	}
+	public void setUserPaymentList(List<UserPayment> userPaymentList) {
+		this.userPaymentList = userPaymentList;
+	}
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		Set<GrantedAuthority> authorites = new HashSet<>();
@@ -119,6 +150,22 @@ public class User implements UserDetails{
 	@Override
 	public boolean isEnabled() {
 		return enabled;
+	}
+	
+	public void setShoppingCart(ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+	
+	public void setUserShippingList(ArrayList<UserShipping> arrayList) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setUserPaymentList(ArrayList<UserPayment> arrayList) {
+		// TODO Auto-generated method stub
+		
+	}
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
 	}
 	
 	
