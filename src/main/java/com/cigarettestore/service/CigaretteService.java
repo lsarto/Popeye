ca665e99993e4527b4e515b80df20fc4d07e9298
LINO\Lino <lino.sarto@gmail.com>
2017-05-20 13:47:58 +0200

    add common\menusAndFilters.html, SearchController per la ricerca dei prodotti tramite filtri.

--- src/main/java/com/cigarettestore/service/CigaretteService.java
+++ src/main/java/com/cigarettestore/service/CigaretteService.java
@@ -6,6 +6,10 @@ import com.cigarettestore.domain.Cigarette;
 
 public interface CigaretteService {
 	List<Cigarette> findAll();
-	
+
 	Cigarette findOne(Long id);
+
+	List<Cigarette> findByCategory(String category);
+
+	List<Cigarette> blurrySearch(String name);
 }
--- src/main/java/com/cigarettestore/service/impl/CigaretteServiceImpl.java
+++ src/main/java/com/cigarettestore/service/impl/CigaretteServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cigarettestore.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,51 @@ import com.cigarettestore.repository.CigaretteRepository;
 import com.cigarettestore.service.CigaretteService;
 
 @Service
-public class CigaretteServiceImpl implements CigaretteService{
+public class CigaretteServiceImpl implements CigaretteService {
 	@Autowired
 	private CigaretteRepository cigaretteRepository;
-	
-	public List<Cigarette> findAll(){
-		return (List<Cigarette>) cigaretteRepository.findAll();
+
+	public List<Cigarette> findAll() {
+		List<Cigarette> cigaretteList = (List<Cigarette>) cigaretteRepository.findAll();
+		List<Cigarette> activeCigaretteList = new ArrayList<>();
+
+		for (Cigarette cigarette : cigaretteList) {
+			if (cigarette.isActive()) {
+				activeCigaretteList.add(cigarette);
+			}
+		}
+
+		return activeCigaretteList;
 	}
 
-	@Override
 	public Cigarette findOne(Long id) {
 		return cigaretteRepository.findOne(id);
+	}
+
+	public List<Cigarette> findByCategory(String category) {
+		List<Cigarette> cigaretteList = cigaretteRepository.findByCategory(category);
+
+		List<Cigarette> activeCigaretteList = new ArrayList<>();
+
+		for (Cigarette cigarette : cigaretteList) {
+			if (cigarette.isActive()) {
+				activeCigaretteList.add(cigarette);
+			}
+		}
+
+		return activeCigaretteList;
+	}
+
+	public List<Cigarette> blurrySearch(String name) {
+		List<Cigarette> cigaretteList = cigaretteRepository.findByNameContaining(name);
+		List<Cigarette> activeCigaretteList = new ArrayList<>();
+
+		for (Cigarette cigarette : cigaretteList) {
+			if (cigarette.isActive()) {
+				activeCigaretteList.add(cigarette);
+			}
+		}
+
+		return activeCigaretteList;
 	}
 }
