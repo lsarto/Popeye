db29a567a8522b5e9ea23c24afff627e77ed4e79
LINO\Lino <lino.sarto@gmail.com>
2017-07-17 18:02:22 +0200

    Add search bar

--- src/main/java/com/cigarettestore/config/SecurityConfig.java
+++ src/main/java/com/cigarettestore/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/fonts/**",
 			"/shop-detail",
 			"/shop-category",
-			"/searchByCategory"
+			"/searchByCategory",
+			"/searchCigarette"
 	};
 
 	@Override
