27025f3c0461781cd0c2f6ec535263e0951c9090
LINO\Lino <lino.sarto@gmail.com>
2017-07-18 16:08:25 +0200

    Add regex in script.js per password e email

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,6 +2,7 @@
  * 
  */
 
+
 function checkBillingAddress() {
 	if($("#theSameAsShippingAddress").is(":checked")) {
 		$(".billingAddress").prop("disabled", true);
@@ -13,6 +14,8 @@ function checkBillingAddress() {
 function checkPasswordMatch() {
 	var password = $("#txtNewPassword").val();
 	var confirmPassword = $("#txtConfirmPassword").val();
+	var regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+	var pattPass = new RegExp(regexPassword);
 	
 	if(password == "" && confirmPassword =="") {
 		$("#checkPasswordMatch").fadeOut();
@@ -23,6 +26,25 @@ function checkPasswordMatch() {
 			$("#checkPasswordMatch").fadeIn();
 			$("#checkPasswordMatch").html("Le password non coincidono!");
 			$("#updateUserInfoButton").prop('disabled', true);
+		} 
+		else if(!pattPass.test(password)) {
+			var passLowerCase = /^(?=.*[a-z])/;
+			var passUpperCase = /^(?=.*[A-Z])/;
+			var passNum = /(?=.*\d)/;
+
+			if(password.length<8){
+				$("#checkPasswordMatch").html("La password deve avere almeno 8 caratteri");
+				$("#updateUserInfoButton").prop('disabled', true);
+			} else if(!(new RegExp(passLowerCase)).test(password)) {
+				$("#checkPasswordMatch").html("La password deve avere almeno una lettera minuscola");
+				$("#updateUserInfoButton").prop('disabled', true);
+			} else if(!(new RegExp(passUpperCase)).test(password)) {
+				$("#checkPasswordMatch").html("La password deve avere almeno una lettera maiuscola");
+				$("#updateUserInfoButton").prop('disabled', true);
+			} else if(!(new RegExp(passNum)).test(password)) {
+				$("#checkPasswordMatch").html("La password deve avere almeno un numero");
+				$("#updateUserInfoButton").prop('disabled', true);
+			}
 		} else {
 			$("#checkPasswordMatch").fadeOut();
 			$("#checkPasswordMatch").html("Le password coincidono");
@@ -31,8 +53,26 @@ function checkPasswordMatch() {
 	}
 }
 
+function checkEmailFormat(){
+	var regexEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	var pattEmail = new RegExp(regexEmail);
+	var emailLogin = $("#email-login").val();
+	
+	if(!pattEmail.test(emailLogin)) {
+		$("#checkEmailFormat").fadeIn();
+		$("#checkEmailFormat").html("Il formato dell'indirizzo email non è corretto");
+		$("#registerButton").prop('disabled', true);
+	}
+	else {
+		$("#checkEmailFormat").fadeOut();
+		$("#checkEmailFormat").html("");
+		$("#registerButton").prop('disabled', false);
+	}
+}
+
 $(document).ready(function(){
 	$("#theSameAsShippingAddress").on('click', checkBillingAddress);
 	$("#txtConfirmPassword").keyup(checkPasswordMatch);
 	$("#txtNewPassword").keyup(checkPasswordMatch);
+	$("#email-login").keyup(checkEmailFormat); //validazione email in customer-register.html
 });
